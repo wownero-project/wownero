45c1adb5789afadc631bde302e5ddb044e82e368
wowario <wowario@protonmail.com>
2021-05-30 14:40:02 +0300

    miner block header signing

--- src/cryptonote_basic/cryptonote_basic.h
+++ src/cryptonote_basic/cryptonote_basic.h
@@ -461,6 +461,7 @@ namespace cryptonote
     uint64_t timestamp;
     crypto::hash  prev_id;
     uint32_t nonce;
+    crypto::signature signature;
 
     BEGIN_SERIALIZE()
       VARINT_FIELD(major_version)
@@ -468,6 +469,8 @@ namespace cryptonote
       VARINT_FIELD(timestamp)
       FIELD(prev_id)
       FIELD(nonce)
+      if (major_version >= HF_VERSION_BLOCK_HEADER_MINER_SIG)
+          FIELD(signature)
     END_SERIALIZE()
   };
 

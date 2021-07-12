77060ae43585f009b8a3805186d52793b2cd3253
wowario <wowario@protonmail.com>
2021-07-12 20:31:04 +0300

    remove DNS ips

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -45,11 +45,6 @@ using namespace epee;
 
 static const char *DEFAULT_DNS_PUBLIC_ADDR[] =
 {
-  "194.150.168.168",    // CCC (Germany)
-  "80.67.169.40",       // FDN (France)
-  "89.233.43.71",       // http://censurfridns.dk (Denmark)
-  "109.69.8.51",        // punCAT (Spain)
-  "193.58.251.251",     // SkyDNS (Russia)
 };
 
 static boost::mutex instance_lock;

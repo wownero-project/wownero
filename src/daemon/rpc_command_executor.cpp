34bc7d2b63c7c71e8a39139b2100a075712cc971
wowario <wowario@protonmail.com>
2023-01-25 19:41:15 +0300

    show full version

--- src/daemon/rpc_command_executor.cpp
+++ src/daemon/rpc_command_executor.cpp
@@ -2533,7 +2533,7 @@ bool t_rpc_command_executor::version()
         }
     }
 
-    if (res.version.empty() || !cryptonote::rpc::is_version_string_valid(res.version))
+    if (res.version.empty())
     {
         tools::fail_msg_writer() << "The daemon software version is not available.";
     }

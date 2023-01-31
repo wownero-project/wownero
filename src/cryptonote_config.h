bf12e2745dbddf665c4102a3abe02e7b1b8519f1
wowario <wowario@protonmail.com>
2023-01-31 23:06:50 +0300

    limit future blk time to 10 min

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -46,6 +46,7 @@
 #define CURRENT_TRANSACTION_VERSION                     2
 #define CURRENT_BLOCK_MAJOR_VERSION                     7
 #define CURRENT_BLOCK_MINOR_VERSION                     7
+#define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V2           300*2
 #define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60*60*2
 #define CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE             4
 

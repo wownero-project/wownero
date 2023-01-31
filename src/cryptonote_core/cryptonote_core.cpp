e8e42507adff129f62a3e55884a41c2344ca5c23
wowario <wowario@protonmail.com>
2023-01-31 10:22:40 +0300

    set quick height for syncing

--- src/cryptonote_core/cryptonote_core.cpp
+++ src/cryptonote_core/cryptonote_core.cpp
@@ -934,7 +934,7 @@ namespace cryptonote
   //-----------------------------------------------------------------------------------------------
   size_t core::get_block_sync_size(uint64_t height) const
   {
-    static const uint64_t quick_height = m_nettype == TESTNET ? 801219 : m_nettype == MAINNET ? 1220516 : 0;
+    static const uint64_t quick_height = m_nettype == TESTNET ? 0 : m_nettype == MAINNET ? 53666 : 0;
     size_t res = 0;
     if (block_sync_size > 0)
       res = block_sync_size;
